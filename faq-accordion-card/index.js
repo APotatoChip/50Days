d5da9c218dc7b1e1e48757ddb40ece2c988acbc1
APotatoChip <cvetina.a.m@gmail.com>
2021-06-21 16:22:02 +0300

    accordion faq moving title fixed

--- faq-accordion-card/index.js
+++ faq-accordion-card/index.js
@@ -1,35 +1,13 @@
-const questions = document.querySelectorAll('.question');
-const arrQ = Array.from(questions);
-const answers = document.querySelectorAll('.answer');
-const arrA = Array.from(answers);
-const arrows = document.querySelectorAll('.arrow');
-const arrArrows = Array.from(arrows);
-answers.forEach(ans => ans.classList.add('hide'));
-
-document.addEventListener('click', (e) => {
-    const currClass = e.target.parentNode.classList[0];
-    if (currClass === 'question') {
-        let currQ = e.target.parentNode;
-        let idx = arrQ.indexOf(currQ);
-        let currA = arrA[idx];
-        let h3 = arrQ[idx].childNodes[1];
-        let currArrow = arrArrows[idx];
-
-        currA.classList.toggle('hide');
-        h3.classList.toggle('black');
-        currArrow.classList.toggle('rotate');
-
-
-        for (let i = 0; i < idx; i++) {
-            arrA[i].classList.add('hide');
-            arrQ[i].childNodes[1].classList.remove('black');
-            arrArrows[i].classList.remove('rotate');
+const qs = document.querySelectorAll('.question');
+qs.forEach((q) => {
+    q.addEventListener('click', () => {
+            if (q.parentNode.classList.contains('active')) {
+                q.parentNode.classList.toggle('active');
+            } else {
+                qs.forEach(q => q.parentNode.classList.remove('active'));
+                q.parentNode.classList.add('active');
+            }
         }
-        for (let i = idx + 1; i < arrQ.length; i++) {
-            arrA[i].classList.add('hide');
-            arrQ[i].childNodes[1].classList.remove('black');
-            arrArrows[i].classList.remove('rotate');
-        }
-    }
 
+    )
 });
